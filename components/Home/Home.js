1179ac5b26aa17a6e4962d17aa3a2c8b84150c1b
BriceKaszluk <kaszlukb@gmail.com>
2021-10-06 01:34:33 +0200

    emotions component first part

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from './styles/Home.module.scss';
+import EmotionRail from '../common/EmotionsRail';
 
 export default function Home() {
   return (
@@ -16,6 +17,8 @@ export default function Home() {
         <h1 className={styles.main_title}>
           WeNeedLove
         </h1>
+
+        <EmotionRail />
 
         <span className={styles.description_1}>
           you are a good person never forget it
