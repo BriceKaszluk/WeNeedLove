1a5eab0096b7bc933f8a5f39d29a1511ce9341d5
BriceKaszluk <kaszlukb@gmail.com>
2021-10-10 17:43:26 +0200

    gtm

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/globals.scss';
 import MainLayout from '../components/MainLayout/MainLayout';
 import { supabase } from '../services/supabaseClient';
 import { useRouter } from "next/router";
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
 
@@ -38,11 +39,27 @@ function MyApp({ Component, pageProps }) {
 
   return(
     <>
+      {/* <!-- Google Tag Manager --> */}
+      <Script 
+      id="google_tag_manager"
+      dangerouslySetInnerHTML={{
+    __html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5T4KV72');`}} />
+
     {
       (router?.asPath && !notRedirectingUrl.includes(router.asPath) && !session) ? 'loading' :  
       <MainLayout 
         session={session}
       >
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5T4KV72"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         <Component {...pageProps} />
       </MainLayout>
     }
