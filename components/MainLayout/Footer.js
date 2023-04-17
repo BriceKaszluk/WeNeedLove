b5949f9dc9a264f32256f33c772ae21dc86258c1
BriceKaszluk <kaszlukb@gmail.com>
2023-04-17 11:38:36 +0200

    update mail adress

--- components/MainLayout/Footer.js
+++ components/MainLayout/Footer.js
@@ -4,7 +4,7 @@ import styles from './styles/Footer.module.scss';
 export default function Footer() {
   return(
     <footer className="flex_column_centered">
-      <a className={styles.contact_link} href="mailto:weneedlove.contact@gmail.com">weneedlove.contact@gmail.com</a>
+      <a className={styles.contact_link} href="mailto:webvista.developpeur@gmail.com">weneedlove.contact@gmail.com</a>
     </footer>
   )
 }
