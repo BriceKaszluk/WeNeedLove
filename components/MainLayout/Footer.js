93fb0c5681ec3e6e252781751626da1ee597ee91
BriceKaszluk <kaszlukb@gmail.com>
2023-06-01 23:17:08 +0200

    fix footer

--- components/MainLayout/Footer.js
+++ components/MainLayout/Footer.js
@@ -6,12 +6,12 @@ export default function Footer() {
 <footer className="bg-gray-800 text-white px-6 py-4">
   <div className="container mx-auto flex flex-wrap justify-between items-center">
     <div className="w-full md:w-auto mb-4 md:mb-0 text-center md:text-left">
-      <a href="mailto:webvista.developpeur@gmail.com" className="text-lg font-semibold">weneedlove.contact@gmail.com</a>
+      <a href="mailto:webvista.developpeur@gmail.com" className="text-lg font-semibold">Contact</a>
     </div>
     <nav className="w-full md:w-auto text-center md:text-right">
-      <a href="/piggy-bank" className="text-gray-400 hover:text-white ml-0 md:ml-4 mt-2 md:mt-0 block md:inline-block">Piggy Bank</a>
-      <a href="/give-love" className="text-gray-400 hover:text-white ml-0 md:ml-4 mt-2 md:mt-0 block md:inline-block">Give Love</a>
-      <a href="/need-love" className="text-gray-400 hover:text-white ml-0 md:ml-4 mt-2 md:mt-0 block md:inline-block">Need Love</a>
+      <a href="/piggy-bank" className="text-gray-400 hover:text-white ml-0 md:ml-4 mt-2 md:mt-0 block md:inline-block">Mon espace</a>
+      <a href="/give-love" className="text-gray-400 hover:text-white ml-0 md:ml-4 mt-2 md:mt-0 block md:inline-block">Mes commentaires</a>
+      <a href="/need-love" className="text-gray-400 hover:text-white ml-0 md:ml-4 mt-2 md:mt-0 block md:inline-block">Mes histoires</a>
       <a href="/cgu-cgv" className="text-gray-400 hover:text-white ml-0 md:ml-4 mt-2 md:mt-0 block md:inline-block">CGV/CGU</a>
     </nav>
   </div>
--- components/NeedLove/MainLayout.js
+++ components/NeedLove/MainLayout.js
@@ -7,7 +7,7 @@ export default function MainLayout() {
   return (
 <div className="flex items-center justify-center">
   <div className="container mx-auto p-4 md:p-8 max-w-screen-lg bg-white rounded-lg shadow-md">
-    <h1 className="text-3xl font-bold text-indigo-600 mb-4">Besoin d'en parler ?</h1>
+    <h1 className="text-3xl font-bold text-indigo-600 mb-4">Mes histoires</h1>
     <p className="text-lg text-gray-700 mb-6">
       <strong>Raconte-nous ton histoire</strong>, la communauté te soutiendra avec bienveillance, 
       <strong>ne t'inquiète pas c'est anonyme😎 </strong>!
