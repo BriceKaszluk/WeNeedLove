cd9fc7de7ed5fc8ccbc37c2abc15acb108843ac0
BriceKaszluk <kaszlukb@gmail.com>
2023-06-02 02:29:19 +0200

    fix button next on give-love

--- components/GiveLove/OneStoryCard.js
+++ components/GiveLove/OneStoryCard.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faForward } from "@fortawesome/free-solid-svg-icons";
 
-export default function OneStoryCard({story, fetchRandomStory}) {
-
+export default function OneStoryCard({ story, fetchRandomStory }) {
   return (
-<div className="relative p-4 bg-white rounded-lg shadow-lg mb-8 border-2">
-  <h3 className="text-xl font-bold text-indigo-600 mb-2">{story.title}</h3>
-  <p className="text-lg text-gray-700 mb-2">{story.text}</p>
-  <button
-    className="absolute top-2 right-2 w-10 h-10 flex justify-center items-center p-2 bg-indigo-600 rounded-full text-white hover:bg-indigo-500 transition-colors duration-300"
-    onClick={() => fetchRandomStory()}
-  >
-  <FontAwesomeIcon icon={faForward} />
-  </button>
-</div>
+    <div className="relative p-4 bg-white rounded-lg shadow-lg mb-8 border-2">
+      <div className="flex justify-between items-start w-full">
+        <h3 className="text-xl font-bold text-indigo-600 mb-2">
+          {story.title}
+        </h3>
+        <button
+          className=" w-8 h-8 flex justify-center items-center p-2 bg-indigo-600 rounded-full text-white hover:bg-indigo-500 transition-colors duration-300"
+          onClick={() => fetchRandomStory()}
+        >
+          <FontAwesomeIcon icon={faForward} />
+        </button>
+      </div>
 
-
-
-
-  )
+      <p className="text-lg text-gray-700 mb-2">{story.text}</p>
+    </div>
+  );
 }
